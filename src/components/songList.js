5c928d8d25562d598bebe2eada2189adfa8b49a0
Dan Zera <dczera@gmail.com>
2020-05-26 17:35:27 -0500

    utilize mapStateToProps to give songList comp access to songs from redux store, add getSongList function to generate JSX for list of songs

--- src/components/songList.js
+++ src/components/songList.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends Component {
+	getSongList() {
+		return this.props.songs.map((song, index) => {
+			return (
+				<div className="song" key={index}>
+					{song.title}
+				</div>
+			);
+		});
+	}
+
 	render() {
+		const songList = this.getSongList();
+		
 		return (
 			<div className="song-list">
-				SongList
+				{songList}
 			</div>
 		);
 	}
 }
 
-export default SongList
+// this function is conventionally named mapStateToProps (not required)
+// can be a function keyword or an arrow function
+// it receives all of the state within the redux store as an arg
+const mapStateToProps = (state) => {
+	// the object returned from this function gets injected into our component as props
+	// i.e. this.props === { songs: state.songs }
+	// this is how we get data from our redux store in our components
+	return {
+		songs: state.songs
+	};
+}
+
+// connect is a function that returns a function
+// need to call the connect function via connect()
+// and invoke the function that is returned via (SongList)
+export default connect(mapStateToProps)(SongList);
