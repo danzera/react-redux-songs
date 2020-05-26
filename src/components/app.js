9a5ac594a2441086c7ac1830cb8d2cd29fab3799
Dan Zera <dczera@gmail.com>
2020-05-25 23:00:21 -0500

    add baase SongList component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // webpack automatically imports the index.js file, just need to reference the directory, not the file
 import actions from '../actions';
+import SongList from './songList';
 
 const App = () => {
 	return (
 		<div className="app">
-			Hey, there.
+			<SongList />
 		</div>
 	);
 }
---
+++ src/components/songList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+	render() {
+		return (
+			<div className="song-list">
+				SongList
+			</div>
+		);
+	}
+}
+
+export default SongList
